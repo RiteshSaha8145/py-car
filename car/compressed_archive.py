7418214027f9eb7632687c958c871e2fa9de00e5
RiteshSaha8145 <riteshsaha659@gmail.com>
2024-04-01 21:26:16 +0530

    ref: document class

--- car/compressed_archive.py
+++ car/compressed_archive.py
@@ -1,6 +1,13 @@
-from .file import File
+from ..abstract.file import File
+from contextlib import AbstractContextManager
 
 
-class CARFile:
-    def __init__(self, file: File) -> None:
-        self.file: File = file
+class CARv1Reader(AbstractContextManager):
+    def __init__(self, file: File):
+        self.file = file
+
+
+class CARv1Writer(AbstractContextManager):
+    def __init__(self, file: File, name: str):
+        self.file = file
+        self.name = name
