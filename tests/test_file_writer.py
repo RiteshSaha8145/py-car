90e7aa4fe8062bc78046514e480cbec1deafb99f
Ritesh7766 <riteshsaha214@gmail.com>
2024-06-01 15:03:09 +0530

    fix: fix paths

--- tests/test_file_writer.py
+++ tests/test_file_writer.py
@@ -2,15 +2,25 @@ from pycar.car import CARv1Writer
 from pycar.file_types import BinaryFile
 import pytest
 
+from pathlib import Path
 
-def test_carv1_file_writer():
+
+@pytest.fixture
+def dummy_file_path():
+    # Get the path to the current test file
+    test_file_path = Path(__file__).parent
+    # Construct the full path to the dummy file
+    return test_file_path / "statics" / "dummy"
+
+
+def test_carv1_file_writer(dummy_file_path):
     try:
-        with open("dummy", "rb") as f:
+        with open(dummy_file_path, "rb") as f:
             with CARv1Writer(
                 BinaryFile(bufferedReader=f, chunkSize=1, metadata={"name": "dummy"}),
                 "file.car",
                 unixfs=True,
-                max_children=11,
+                max_children=1024,
             ) as c:
                 c.get_car()
     except Exception as e:
--- tests/test_folder_writer.py
+++ tests/test_folder_writer.py
@@ -1,15 +1,24 @@
 from pycar.car import folder_to_dag, CARv1Writer
 import pytest
+from pathlib import Path
 
 
-def test_carv1_file_writer():
+@pytest.fixture
+def dummy_folder_path():
+    # Get the path to the current test file
+    test_file_path = Path(__file__).parent
+    # Construct the full path to the dummy file
+    return test_file_path / "statics" / "dummy_folder"
+
+
+def test_carv1_file_writer(dummy_folder_path):
     try:
         with CARv1Writer(
             None,
             "dummy_folder.car",
             unixfs=True,
-            max_children=11,
+            max_children=1024,
         ) as c:
-            folder_to_dag(car_writer=c, folder_path="./statics", chunk_size=1)
+            folder_to_dag(car_writer=c, folder_path=dummy_folder_path, chunk_size=1)
     except Exception as e:
         pytest.fail(f"Test failed due to unexpected exception: {e}")
